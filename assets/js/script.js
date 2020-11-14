4226ec8de4a7f5bbf847e16ad3b7051635ede73d
DimitriNakos <dimi1nakos@gmail.com>
2020-11-13 22:10:26 -0500

    added comments to the code for readability.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 
-
+//  Set up variables
 var chosenCity = $("#chosenCity")
 var chosenCityTitle = $("#chosenCityTitle")
 var chosenCityTemp = $("#chosenCityTemp")
@@ -23,7 +23,7 @@ function ajaxRetrieval(chosenCity) {
 
         var imageURL = "https://openweathermap.org/img/wn/" + response.weather[0].icon + ".png"
         var image = $("<img src='" + imageURL + "'>")
-
+        // set the dashboard for the current city selected
         chosenCityTitle.html(response.name + "  -  " + moment().format("MMM Do YYYY") + "  -  ")
         chosenCityTitle.append(image)
         chosenCityTemp.text("Temperature: " + temp.toFixed(2) + "℉")
@@ -32,7 +32,7 @@ function ajaxRetrieval(chosenCity) {
         UVindexPlaceHolder.html("UV Index: <span id='chosenCityUV'></span>")
 
         var UVIndexURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + long + "&appid=" + APIkey;
-
+        // if statement for the UV index color
         $.ajax({ url: UVIndexURL, method: "GET" }).then(function (newResponse) {
             var chosenCityUV = $("#chosenCityUV")
             chosenCityUV.text(newResponse.value)
@@ -54,7 +54,7 @@ function ajaxRetrieval(chosenCity) {
     fillInfiveDay(chosenCity)
 }
 
-
+// this section fill sin the forecast div
 function fillInfiveDay(chosenCity) {
     var APIkey = "93048a14e536394603a5f5173a41d761"
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + chosenCity + "&units=imperial&appid=" + APIkey
@@ -80,20 +80,19 @@ function fillInfiveDay(chosenCity) {
     })
 }
 
+// controls the action from the submit button or enter button
 $("#citySubmit").on("submit", function (event) {
     event.preventDefault();
     var chosenCity = $("#cityName").val().trim()
     hidden.removeClass("hidden")
 
-
-
     cityList.push(chosenCity)
     localStorage.setItem("City", chosenCity)
-
 
     renderList()
 })
 
+//  renders the past search results.
 function renderList() {
     $(".citiesSaved").empty()
     for (let i = 0; i < cityList.length; i++) {
@@ -102,24 +101,22 @@ function renderList() {
 
         pastCity.addClass("btn btn-dark cityName mb-2")
         $(".citiesSaved").prepend(pastCity)
-
     }
     ajaxRetrieval(encodeURIComponent(cityList[cityList.length - 1]))
 }
 
+//  Controls the click on the past cities selected buttons
 $(document).on("click", ".cityName", function () {
     ajaxRetrieval(encodeURIComponent($(this).attr("data-city")))
-    cityList.push(chosenCity)
     localStorage.setItem("City", $(this).attr("data-city"))
-
 })
 
+//  checks initial localstorage and populates if there is content to upload
 function init() {
     if (!localStorage.getItem("City")) {
         return
     }
     hidden.removeClass("hidden")
     ajaxRetrieval(localStorage.getItem("City"))
-
 }
 init()
