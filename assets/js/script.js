dd510e598718320600d1c840a69298991889c3bb
DimitriNakos <dimi1nakos@gmail.com>
2020-11-13 19:00:36 -0500

    added little images and some styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,18 +7,11 @@ var chosenCityHumid = $("#chosenCityHumid")
 var chosenCityWind = $("#chosenCityWind")
 var chosenCityUV = $("span")
 var UVindexPlaceHolder = $("#UVindexPlaceHolder")
-
-// remove after logic formed
-var chosenCity
-
+var cityList = []
 var APIkey = "93048a14e536394603a5f5173a41d761"
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + chosenCity + "&appid=" + APIkey;
 
 //  Current weather information
-function ajaxRetrieval() {
-
-    chosenCity = $("#cityName").val().split(' ').join('+')
-    var APIkey = "93048a14e536394603a5f5173a41d761"
+function ajaxRetrieval(chosenCity) {
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + chosenCity + "&appid=" + APIkey;
 
     $.ajax({ url: queryURL, method: "GET" }).then(function (response) {
@@ -26,7 +19,13 @@ function ajaxRetrieval() {
         var long = parseInt(response.coord.lon)
         var lat = parseInt(response.coord.lat)
 
-        chosenCityTitle.text(response.name + "  -  " + moment().format("MMM Do YYYY"))
+        console.log(response.weather[0].icon)
+
+        var imageURL = "http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png"
+        var image = $("<img src='" + imageURL + "'>")
+
+        chosenCityTitle.html(response.name + "  -  " + moment().format("MMM Do YYYY") + "  -  ")
+        chosenCityTitle.append(image)
         chosenCityTemp.text("Temperature: " + temp.toFixed(2) + "℉")
         chosenCityHumid.text("Humidity: " + response.main.humidity + "%")
         chosenCityWind.text("Wind speed: " + response.wind.speed + " mph")
@@ -52,102 +51,62 @@ function ajaxRetrieval() {
             }
         });
     });
+    fillInfiveDay(chosenCity)
 }
 
-//  search input
-//  $("#cityName").val()  grabs the value inside the text area
-$("#citySubmit").on("click", function (event) {
+$("#citySubmit").on("submit", function (event) {
     event.preventDefault();
-    ajaxRetrieval();
-    fiveDayGen()
-    fillInfiveDay()
+    var chosenCity = $("#cityName").val().trim()
+    console.log(chosenCity)
+    cityList.push(chosenCity)
+    renderList()
 })
 
-$("#cityName").on("keypress", function (event) {
-    if (event.which == 13 && !event.shiftKey) {
-        event.preventDefault();
-        ajaxRetrieval();
-        fiveDayGen()
-        fillInfiveDay()
-    }
-});
-
-function localStorageCities() {
-
-}
-
-function fiveDayGen() {
-    for (let i = 0; i < 5; i++) {
-        var fiveDayDiv = $("<div class='col-md-2' id='" + i + "' />")
-        $("#fiveDay").append(fiveDayDiv)
-        $("#" + i).append("<li id='liDate" + i + "' />")
-        $("#" + i).append("<li id='liImg" + i + "' />")
-        $("#" + i).append("<li id='liTemp" + i + "' />")
-        $("#" + i).append("<li id='liHumid" + i + "' />")
-    }
-}
-
-function fillInfiveDay() {
-    chosenCity = $("#cityName").val().split(' ').join('+')
+function fillInfiveDay(chosenCity) {
     var APIkey = "93048a14e536394603a5f5173a41d761"
-    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + chosenCity + "&appid=" + APIkey
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + chosenCity + "&units=imperial&appid=" + APIkey
 
     $.ajax({ url: queryURL, method: "GET" }).then(function (forecast) {
-        $("#liDate0").text(forecast.list[0].dt_txt.substring(0, 10))
-
-        $("#liImg0").text(forecast.list[0])
-
-        $("#liTemp0").text(forecast.list[0].main.temp)
-        $("#liHumid0").text(forecast.list[0].main.humidity)
-
         console.log(forecast)
-    })
+        $("#fiveDay").empty()
+        for (let i = 0; i < forecast.list.length; i++) {
+            var thisWeather = forecast.list[i]
+            if (thisWeather.dt_txt.split(" ")[1] === "15:00:00") {
+                console.log(thisWeather)
+                var col = $("<div class='col-md-2'>")
+                col.addClass("bg-primary card mr-1")
+                var date = $("<div>").text(thisWeather.dt_txt.substring(0, 10))
 
-    $.ajax({ url: queryURL, method: "GET" }).then(function (forecast) {
-        $("#liDate1").text(forecast.list[7].dt_txt.substring(0, 10))
+                var weather = $("<img>").attr("src", `http://openweathermap.org/img/wn/${thisWeather.weather[0].icon}.png`)
+                weather.attr("width", "50")
+                var temp = $("<div>").text("Temp: " + thisWeather.main.temp + "℉")
+                var humid = $("<div>").text("Humidity: " + thisWeather.main.humidity)
+                col.append(date, weather, temp, humid)
 
-        $("#liImg1").text(forecast.list[7])
-
-        $("#liTemp1").text(forecast.list[7].main.temp)
-        $("#liHumid1").text(forecast.list[7].main.humidity)
-    })
-
-    $.ajax({ url: queryURL, method: "GET" }).then(function (forecast) {
-        $("#liDate2").text(forecast.list[15].dt_txt.substring(0, 10))
-
-        $("#liImg2").text(forecast.list[15])
-
-        $("#liTemp2").text(forecast.list[15].main.temp)
-        $("#liHumid2").text(forecast.list[15].main.humidity)
-    })
-
-    $.ajax({ url: queryURL, method: "GET" }).then(function (forecast) {
-        $("#liDate3").text(forecast.list[23].dt_txt.substring(0, 10))
-
-        $("#liImg3").text(forecast.list[23])
-
-        $("#liTemp3").text(forecast.list[23].main.temp)
-        $("#liHumid3").text(forecast.list[23].main.humidity)
-    })
-
-    $.ajax({ url: queryURL, method: "GET" }).then(function (forecast) {
-        $("#liDate4").text(forecast.list[31].dt_txt.substring(0, 10))
-
-        $("#liImg4").text(forecast.list[31])
-
-        $("#liTemp4").text(forecast.list[31].main.temp)
-        $("#liHumid4").text(forecast.list[31].main.humidity)
+                $("#fiveDay").append(col)
+            }
+        }
     })
 }
 
+function renderList() {
+    $(".citiesSaved").empty()
+    for (let i = 0; i < cityList.length; i++) {
+        console.log("render")
+        var pastCity = $("<button>").text(cityList[i])
+        pastCity.attr("data-city", cityList[i])
 
-//  create 5 col-md-1 divs inside 5 day forecast
+        pastCity.addClass("btn btn-dark cityName mb-2")
+        $(".citiesSaved").prepend(pastCity)
 
-//  Button saving
+    }
+    ajaxRetrieval(encodeURIComponent(cityList[cityList.length - 1]))
+}
 
-//  outputting to chosen city
+$(document).on("click", ".cityName", function () {
+    ajaxRetrieval(encodeURIComponent($(this).attr("data-city")))
+})
 
-//  List of cities on local storage
+function localStorageCities(chosenCity) {
 
-//  create the chosen city card when an input is put in
-// fiveDayGen()
+}
